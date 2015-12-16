325903ce5535f41ec48011b2dff31783ffd90814
Jordi Prats <jprats@systemadmin.es>
2015-12-16 14:27:28 +0100

    inicial

---
+++ manifests/logs.pp
@@ -0,0 +1,28 @@
+define logrotate::logs(
+                        $log,
+                        $namelog=$name,
+                        $ensure = 'present',
+                        $su=undef,
+                        $rotate=undef,
+                        $maxage=undef,
+                        $compress=undef,
+                        $delaycompress=undef,
+                        $notifempty=undef,
+                        $frequency=undef,
+                        $missingok=undef,
+                        $postrotate=undef,
+                        $dateext=false,
+                        $copytruncate=false,
+                      ) {
+
+  #
+  file { "${logrotate::params::puppet_managed_dir}/${namelog}":
+    ensure => $ensure,
+    owner => 'root',
+    group => 'root',
+    mode => '0644',
+    require => [ Package[$logrotate::params::package_logrotate], File[$logrotate::params::puppet_managed_dir] ],
+    content => template("${module_name}/logfile_template.erb")
+  }
+
+}
---
+++ manifests/params.pp
@@ -0,0 +1,40 @@
+class logrotate::params {
+
+	$package_logrotate='logrotate'
+	$puppet_managed_dir='/etc/logrotate.d/puppet-managed'
+
+	case $::osfamily
+	{
+		'redhat':
+    		{
+			case $::operatingsystemrelease
+			{
+				/^[67].*$/:
+				{
+				}
+				default: { fail("Unsupported RHEL/CentOS version! - $::operatingsystemrelease")  }
+			}
+
+		}
+		'Debian':
+		{
+			case $::operatingsystem
+			{
+				'Ubuntu':
+				{
+					case $::operatingsystemrelease
+					{
+						/^14.*$/:
+						{
+              $su_default='root syslog'
+						}
+						default: { fail("Unsupported Ubuntu version! - $::operatingsystemrelease")  }
+					}
+				}
+				'Debian': { fail("Unsupported")  }
+				default: { fail("Unsupported Debian flavour!")  }
+			}
+		}
+		default: { fail("Unsupported OS!")  }
+	}
+}
