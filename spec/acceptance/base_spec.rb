6753965f174df4cf79b8b8255d8fe1383259b80d
Jordi Prats <jprats@systemadmin.es>
2016-08-16 13:54:41 +0200

    mes checks

--- spec/acceptance/base_spec.rb
+++ spec/acceptance/base_spec.rb
@@ -17,6 +17,14 @@ describe 'logrotate class' do
       expect(apply_manifest(pp).exit_code).to eq(0)
     end
 
+    #/etc/logrotate.conf
+    describe file($baseconf) do
+      it { should be_file }
+      its(:content) { should match 'puppet managed' }
+      its(:content) { should match 'include /etc/logrotate.d/puppet-managed' }
+      its(:content) { should match '# DO NOT CONFIGURE system-specific logs here' }
+    end
+
   end
 
 end
--- spec/acceptance/version.rb
+++ spec/acceptance/version.rb
@@ -6,10 +6,13 @@ _operatingsystemrelease = fact('operatingsystemrelease').to_f
 case _osfamily
 when 'RedHat'
   $packagename     = 'logrotate'
+  $baseconf        = '/etc/logrotate.conf'
 
 when 'Debian'
   $packagename     = 'logrotate'
+  $baseconf        = '/etc/logrotate.conf'
 
 else
   $packagename     = '-_-'
+  $baseconf        = '-_-'
 end
