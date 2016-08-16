3e90ebca48b9fb6423b542261ef39f45ba3b5e8a
Jordi Prats <jprats@systemadmin.es>
2016-08-16 13:56:13 +0200

    afgegit base exampel

---
+++ spec/acceptance/example_spec.rb
@@ -0,0 +1,56 @@
+require 'spec_helper_acceptance'
+require_relative './version.rb'
+
+describe 'logrotate class' do
+
+  context 'basic setup' do
+    # Using puppet_apply as a helper
+    it 'should work with no errors' do
+      pp = <<-EOF
+
+      class { 'logrotate': }
+
+      logrotate::logs { 'example':
+        create_mode => '0777',
+        rotate      => '3',
+      }
+
+      logrotate::logs { 'bitban1':
+        log           => '/deploy/VAR/local/elespanol/www.elespanol.com/BB3Logs/*log*',
+        su            => [ 'bbt-deploy', 'bbt-deploy' ],
+        rotate        => '2',
+        frequency     => 'daily',
+        notifempty    => true,
+        compress      => true,
+        delaycompress => true,
+        missingok     => true,
+      }
+
+      logrotate::logs { 'rotatnginx':
+        log        => '/LOGS/web/*.log',
+        rotate     => '2',
+        frequency  => 'daily',
+        notifempty => true,
+        compress   => true,
+        missingok  => true,
+        postrotate => '/etc/init.d/nginx reload',
+      }
+
+      EOF
+
+      # Run it twice and test for idempotency
+      expect(apply_manifest(pp).exit_code).to_not eq(1)
+      expect(apply_manifest(pp).exit_code).to eq(0)
+    end
+
+    #/etc/logrotate.conf
+    describe file($baseconf) do
+      it { should be_file }
+      its(:content) { should match 'puppet managed' }
+      its(:content) { should match 'include /etc/logrotate.d/puppet-managed' }
+      its(:content) { should match '# DO NOT CONFIGURE system-specific logs here' }
+    end
+
+  end
+
+end
