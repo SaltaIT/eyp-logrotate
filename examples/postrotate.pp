7c18dd49debb0b8d56c81623fb504a9ea9b1cab1
Jordi Prats <jordi.prats@gmail.com>
2018-11-13 13:56:21 +0100

    lint

--- examples/postrotate.pp
+++ examples/postrotate.pp
@@ -1,9 +1,11 @@
 class { 'logrotate': }
 
-logrotate::logs { '/demo.log':
+logrotate::logs { 'single line postrotate':
+  log        => '/demo.log',
   postrotate => '/demo.sh'
 }
 
-logrotate::logs { '/demo_multi.log':
+logrotate::logs { 'multi postrotate':
+  log        => '/demo_multi.log',
   postrotate => [ '/demo1.sh', '/demo2.sh' ],
 }
